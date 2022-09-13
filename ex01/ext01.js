d3ae39f242c0a64db4c241ee9382e498c084979a
Andres Guaita <andres.g@tugerente.com>
2022-09-13 17:59:04 -0500

    Subiendo solucion ejercicio 1

---
+++ ex01/ext01.js
@@ -0,0 +1,11 @@
+
+exports.printCombinations = ()=>{
+    for (let i = 0; i <= 9; i++) {
+        for (let j = i+1; j <= 9; j++) {
+            for (let k = j+1; k <= 9; k++) {
+                console.log(`${i}${j}${k}`);
+            }
+        }
+    }    
+}
+
